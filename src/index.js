0987d41f6533ab5981d44005e9558fffd3263132
Voidless22 <falant221@gmail.com>
2024-11-21 12:50:32 -0500

    add SQL integration skeleton, basic guild id storing on join

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const BotClient = require('./BotClient')
+const utils = require('./utils')
 require('dotenv').config();
 
 
 // Create a new client instance
 const client = new BotClient();
+client.on('guildCreate', async (guild) => {
+    await utils.SQLQuery("CREATE TABLE IF NOT EXISTS `guilds` (guild_id BIGINT, owner_id BIGINT)");
+    await utils.SQLQuery("INSERT INTO guilds (guild_id, owner_id) VALUES (?, ?)", [guild.id, guild.ownerId]);
+    console.log(`Guild ${guild.name} (${guild.id}) has been added to the database.`)
+});
 
 client.loadCommands();
 client.loadEvents();
