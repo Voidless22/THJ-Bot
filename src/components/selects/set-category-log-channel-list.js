12d354c8d3181f2c78a19178ccb5873f08618f86
Voidless22 <falant221@gmail.com>
2024-12-18 21:14:03 -0500

    fix another location of same edge case

--- src/components/selects/set-category-log-channel-list.js
+++ src/components/selects/set-category-log-channel-list.js
@@ -9,13 +9,13 @@ module.exports = {
     run: async (client, interaction) => {
         let selectedCategory = interaction.values[0]
         let categoryChannelSelect = new ChannelSelectMenuBuilder()
-            .setCustomId('category-log-channel-list')
+            .setCustomId(`${selectedCategory}-log-channel-list`)
             .setChannelTypes([ChannelType.GuildText, ChannelType.GuildForum]);
         let actionRow = new ActionRowBuilder().addComponents(categoryChannelSelect);
         
         const collector = interaction.channel.createMessageComponentCollector({ time: 150000 });
         collector.on('collect', async action => {
-            if (action.customId === 'category-log-channel-list') {
+            if (action.customId === `${selectedCategory}-log-channel-list`) {
                 SQLUtils.SQLQuery('UPDATE tickettype SET ticket_log_channel = ? where name IN (?);', [action.values[0], selectedCategory])
                 action.reply({content: 'Success', ephemeral: true})
                 collector.stop();
