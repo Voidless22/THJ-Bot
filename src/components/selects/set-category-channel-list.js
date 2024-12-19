94cf4137f9b229a9e472e654b2cf23ae10499cd8
Voidless22 <falant221@gmail.com>
2024-12-18 21:12:31 -0500

    fix edge case crashing bot when multiple select menus of the same id being created and interacted with. This generated select now has a unique ID

--- src/components/selects/set-category-channel-list.js
+++ src/components/selects/set-category-channel-list.js
@@ -7,22 +7,23 @@ module.exports = {
     customId: 'set-category-channel-list',
 
     run: async (client, interaction) => {
+        
         let selectedCategory = interaction.values[0]
         let categoryChannelSelect = new ChannelSelectMenuBuilder()
-            .setCustomId('category-channel-select-list')
+            .setCustomId(`${selectedCategory}-channel-select-list`)
             .setChannelTypes([ChannelType.GuildText, ChannelType.GuildForum]);
         let actionRow = new ActionRowBuilder().addComponents(categoryChannelSelect);
         
         const collector = interaction.channel.createMessageComponentCollector({ time: 150000 });
         collector.on('collect', async action => {
-            if (action.customId === 'category-channel-select-list') {
+            if (action.customId === `${selectedCategory}-channel-select-list`) {
                 SQLUtils.SQLQuery('UPDATE tickettype SET ticket_recieve_channel = ? where name IN (?);', [action.values[0], selectedCategory])
-                action.reply({content: 'Success', ephemeral: true})
+                await action.reply({content: 'Success', ephemeral: true})
                 collector.stop();
             }
         });
 
-        interaction.reply({ content: `Now let's set the category reception channel.`, components: [actionRow], ephemeral: true })
+        await interaction.reply({ content: `Now let's set the category reception channel.`, components: [actionRow], ephemeral: true })
 
 
     }
